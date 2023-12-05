48810e825aad58c2aa9f4a9aea1c78ec5e691388
erdal2021 <erdal.erdogan@t-online.de>
2023-12-05 23:20:01 +0100

    App updated: modified: README.md main.js package.json templates/index.html

--- README.md
+++ README.md
@@ -1,16 +1,63 @@
-# todo-express
+# JS todoApp express 
 Todo app / javascript + mysql Gruppe 5 
 
-Install/Requirements:
+# Install/Requirements:
+Im root Verzeichnis folgendes ausführen!
+ ```
 npm install express mysql2
+```
 
-App Start: 
+# Datenbank vorbereiten unter Bash
+ Navigiere in das Verzeichnis sql_scripts
+ ```
+cd sql_scripts
+```
+Bereite bzw. erstelle die Datenbank mit folgenden Befehl
+```
+mysql -u root -p < setup_db.sql
+```
+Erstelle die Tabellen für die Datenbank
+```
+mysql -u root -p < setup_table.sql
+```
+Füge die ersten Beispiel-Datensätze in unsere beiden Tabellen ein
+```
+mysql -u root -p < insert_data.sql
+```
+## Hinweis: Falls die Datenbank fehlerhaft ist, versuche die Datenbank mit folgenden Befehl zunächst einmal zu löschen. Starte danach wieder ab Punkt "Datenbank vorbereiten"
+```
+mysql -u root -p < cleanup_db.sql
+```
+
+# Datenbank vorbereiten unter Powershell
+ Navigiere in das Verzeichnis sql_scripts
+ ```
+cd sql_scripts
+```
+Bereite bzw. erstelle die Datenbank mit folgenden Befehl
+```
+Get-Content setup_db.sql | mysql -u root -p
+```
+Erstelle die Tabellen für die Datenbank
+```
+Get-Content setup_table.sql | mysql -u root -p
+```
+Füge die ersten Beispiel-Datensätze in unsere beiden Tabellen ein
+```
+Get-Content insert_data.sql | mysql -u root -p
+```
+## Hinweis: Falls die Datenbank fehlerhaft ist, versuche die Datenbank mit folgenden Befehl zunächst einmal zu löschen. Starte danach wieder ab Punkt "Datenbank vorbereiten"
+```
+Get-Content cleanup_db.sql | mysql -u root -p
+```
+
+# Starten der Anwendung 
+Navigiere zurück ins Verzeichnis
+```
+cd todo-express
+```
+Wir starten die Anwendung mit folgenden Befehl
+```
 node main.js
-
-Connection test on terminal:
-curl -X POST -H "Content-Type:application/json" http://localhost:8000/hello -d '{"test": "text"}'
-
-Response: Hello AWS23-07!
-
-
+```
 
--- main.js
+++ main.js
@@ -1,40 +1,32 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
-// const ejs = require('ejs');
-// const path = require('path');
 
 const app = express();
 const port = 8000;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 
 const DB_USER = "todo";
 const DB_PASSWORD = "1234";
 const DB_HOST = "localhost";
 const DB_NAME = "todosdb";
 
-const templates = "templates";
+// 'templates' folder containing 'index.html'
+app.set('views', 'templates');
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
 
-// express.urlencoded({ extended: true }): Diese Option ermöglicht es, komplexe Objekte und Arrays in das 
-// URL-codierte Format zu kodieren. Wenn sie auf true gesetzt ist, bedeutet dies, dass die 
-// Werte jeden Typ haben können und nicht nur Zeichenketten oder Arrays.
-app.use(express.urlencoded({ extended: true }));
-
-// Serve static files (for example, CSS)
-// app.use('/static', express.static(path.join(__dirname, 'static')));
-
-let connection;
-
-async function createDBConnection() {
+async function createDbConnection() {
     try {
-        connection = await mysql.createConnection({
-            host: DB_HOST,
+        const connection = await mysql.createConnection({
             user: DB_USER,
             password: DB_PASSWORD,
+            host: DB_HOST,
             database: DB_NAME,
         });
         console.log("Connected to the database");
+        return connection;
     } catch (error) {
         console.error(`Error connecting to the database: ${error}`);
         throw error;
@@ -42,32 +34,104 @@ async function createDBConnection() {
 }
 
 async function resetAutoIncrement() {
+    const connection = await createDbConnection();
     const query = "ALTER TABLE todos AUTO_INCREMENT = 1";
-    await connection.execute(query);
+
+    try {
+        await connection.execute(query);
+    } catch (error) {
+        console.error(`Error resetting auto-increment: ${error}`);
+    } finally {
+        connection.end();
+    }
 }
 
+app.get("/", async (req, res) => {
+    const connection = await createDbConnection();
+    const query = "SELECT * FROM todos;";
 
-app.get("/", (req, res) => {
-    console.log("Req:", req);
-    res.send("Hello World!");
+    try {
+        const [rows] = await connection.execute(query);
+        res.render("index", { request: req, todos: rows, app_version: "0.0.1", Lizenz: "Open Source GPL" });
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    } finally {
+        connection.end();
+    }
+});
+
+app.post("/todos", async (req, res) => {
+    const connection = await createDbConnection();
+    const todo = req.body.todo;
+const query = "INSERT INTO todos (item, stat) VALUES (?, ?)";
+    const data = [todo, "open"];
+
+    try {
+        await connection.execute(query, data);
+        res.redirect("/?message=ToDo created successfully");
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    } finally {
+        connection.end();
+    }
+});
+
+app.post("/update_status/:todo_id/:new_status", async (req, res) => {
+    const connection = await createDbConnection();
+    const todoId = req.params.todo_id;
+    const newStatus = req.params.new_status;
+const query = "UPDATE todos SET stat = ? WHERE id = ?";
+    const data = [newStatus, todoId];
+
+    try {
+        await connection.execute(query, data);
+        res.redirect(`/?message=Status of ToDo ${todoId} updated to ${newStatus}`);
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    } finally {
+        connection.end();
+    }
+});
+
+app.post("/update_todo/:todo_id", async (req, res) => {
+    const connection = await createDbConnection();
+    const todoId = req.params.todo_id;
+    const newStatus = req.body.newStatus;
+const query = "UPDATE todos SET stat = ? WHERE id = ?";
+    const data = [newStatus, todoId];
+
+    try {
+        await connection.execute(query, data);
+        res.redirect("/?message=Todo updated successfully");
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    } finally {
+        connection.end();
+    }
+});
+
+app.post("/delete_todo/:todo_id", async (req, res) => {
+    const connection = await createDbConnection();
+    const todoId = req.params.todo_id;
+const query = "DELETE FROM todos WHERE id = ?";
+    const data = [todoId];
+
+    try {
+        await connection.execute(query, data);
+        await resetAutoIncrement();
+        res.redirect(`/?message=ToDo ${todoId} deleted successfully`);
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    } finally {
+        connection.end();
+    }
+});
+
+app.listen(port, () => {
+console.log(`ToDO app started on port ${port}`);
+// console.log("ToDO app started on port " + port);
  })
-
- app.post("/hello", (request, response) => {
-    console.log("Req:", request.body);
-    response.status(200).send("Hello AWS23-07!");
- })
-
-
-
- app.listen(port, async () => {
-    console.log(`Server is running at http://localhost:${port}`);
-    await createDBConnection();
-    await resetAutoIncrement();
-})
-
 
 // app.listen(port, () => {
-//     console.log(`ToDO app started on port ${port}`);
-//     // console.log("ToDO app started on port" + port);
-// })
-
+//     console.log(`Server is running on port ${port}`);
+// });
